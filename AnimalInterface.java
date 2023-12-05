dd0f81163a08089bf399a185fb62de4131a68625
BradpuppyC <rayray21211@gmail.com>
2023-12-05 23:14:06 +0800

    interface miltiple

--- AnimalInterface.java
+++ AnimalInterface.java
@@ -4,7 +4,15 @@ interface Animal1 {
     public void sleep(); // interface method (does not have a body)
 }
 
-class Pig implements Animal1 {
+interface FirstInterface {
+    public void myMethod(); // interface method
+}
+
+interface SecondInterface {
+    public void myOtherMethod(); // interface method
+}
+
+class Pig implements Animal1, FirstInterface, SecondInterface {
     public void animalSound() {
         System.out.println("The pig says: wee wee");
     }
@@ -12,12 +20,22 @@ class Pig implements Animal1 {
     public void sleep() {
         System.out.println("Zzz");
     }
+
+      public void myMethod() {
+    System.out.println("Some text..");
+  }
+  public void myOtherMethod() {
+    System.out.println("Some other text...");
+  }
 }
+
 
 class AnimalInterface {
     public static void main(String[] args) {
         Pig myPig = new Pig();
         myPig.animalSound();
         myPig.sleep();
+        myPig.myMethod();
+        myPig.myOtherMethod();
     }
 }
