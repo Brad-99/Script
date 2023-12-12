788025541166ac0e12a463043952d4dbd5d22180
BradpuppyC <rayray21211@gmail.com>
2023-12-13 00:01:40 +0800

    File1

---
+++ FileDemo/createFile.java
@@ -0,0 +1,31 @@
+package FileDemo;
+
+import java.io.File; // Import the File class
+import java.io.IOException; // Import the IOException class to handle errors
+import java.io.FileWriter;
+
+public class createFile {
+    public static void main(String[] args) {
+        try {
+            File puppyToy = new File("puppyToy.txt");
+            if (puppyToy.createNewFile()) {
+                System.out.println("File created: " + puppyToy.getName());
+            } else {
+                System.out.println("File alrdy exists");
+            }
+        } catch (IOException somethingWrong) {
+            System.out.println("An error occurred");
+            somethingWrong.printStackTrace();
+        }
+        // -------------------------------------------
+        try {
+            FileWriter puppyPen = new FileWriter("puppyToy.txt");
+            puppyPen.write("Bruh Java is easy");
+            puppyPen.close();
+            System.out.println("Successfully wrote to the file");
+        } catch (IOException somethingWrong) {
+            System.out.println("An error occurred");
+            somethingWrong.printStackTrace();
+        }
+    }
+}
---
+++ LambdaDemo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        // ------------------------------------------------------
+        Consumer<Integer> method = (n) -> {
+            System.out.println(n);
+        };
+        // Consumer<Integer> method = new Consumer<Integer>() {
+        // @Override
+        // public void accept(Integer n) {
+        // System.out.println(n);
+        // }
+        // };
+
+        numbers.forEach(method);
+    }
+}
